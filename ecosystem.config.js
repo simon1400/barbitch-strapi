9b01026c57e7225c5c5a3bc71499fc353049b856
Your Name <>
2024-11-24 22:49:45 +0100

    fixed deploy settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'Barbitch client',
+      name: 'Barbitch strapi',
       script: 'npm start',
       env_production: {},
     },
@@ -13,8 +13,8 @@ module.exports = {
       // eslint-disable-next-line sonarjs/no-hardcoded-ip
       host: ['89.221.216.23'],
       ref: 'origin/main',
-      repo: 'git@github.com:simon1400/barbitch-client.git',
-      path: '/home/dimi/app/barbitch/client',
+      repo: 'git@github.com:simon1400/barbitch-strapi.git',
+      path: '/home/dimi/app/barbitch/strapi',
       'post-deploy': 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     },
   },
