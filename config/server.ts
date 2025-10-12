e01b1b0e054d7d2cf493614594b21aad54eacd17
Dmytro Pechunka <pechunka11@gmail.com>
2025-10-12 22:34:48 +0200

    try fix admin

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1350),
-  proxy: true, // 👈 Важно!
-  url: env('PUBLIC_URL', 'https://strapi.barbitch.cz'),
   app: {
     keys: env.array('APP_KEYS'),
   },
