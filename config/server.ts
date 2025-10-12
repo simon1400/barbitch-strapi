4dcae35703190bffabdcfc2d94773d57c513dcb9
Dmytro Pechunka <pechunka11@gmail.com>
2025-10-12 22:56:52 +0200

    add url

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1350),
+  url: env('URL', 'http://localhost:1350'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
